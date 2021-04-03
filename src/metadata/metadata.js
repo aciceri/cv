203fd421d3d63e0c08d9cd84bbd547cfc9b4556a
aciceri <andrea.ciceri@autistici.org>
2021-04-03 19:21:17 +0200

    First rough version

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,12 +1,13 @@
 module.exports = {
     name: 'Andrea Ciceri',
+
     title: 'Full-stack developer',
+
     facts: {
 	'Homepage': `
 <a href="https://en.wikipedia.org/wiki/William_Shakespeare">
 <i class="fa fa-home" aria-hidden="true"></i>
-<span class="print">https://aciceri.dev</span>
-<span class="screen">aciceri.dev</span>
+aciceri.dev
 </a>
 `,
 	'Email': `
@@ -15,37 +16,101 @@ module.exports = {
 	'GitHub': `
 <a href="https://github.com/aciceri"><i class="fa fa-github" aria-hidden="true"></i>aciceri</a>`
     },
-    experience: [
+    
+    experiences: [
 	{
-	    title: 'Hamlet',
+	    title: 'BeatData',
+	    date: '2020 &hyphens; now',
 	    contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+Currently working as software developer at *BeatData*, more info here: [https://beatdata.com](https://beatdata.com)
 `
-	},
-	{
-	    title: 'Hamlet',
+	}, {
+	    title: 'Lunik Cineforum',
+	    date: '2019 &hyphens; now',
 	    contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+I hold the role of vicepresident in the Lunik Cineforum association, a local cineforum. I'm also the technical manager and the creator of the website: [https://lunik.it](https://lunik.it)
 `
-	},
-	{
-	    title: 'Hamlet',
+	}, {
+	    title: 'Cooperativa Edificatrice Cernuschese',
+	    date: '2017 &hyphens; now',
 	    contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+I'm a member of the Board of Directors of a local building cooperative, in addition to carrying out the most disparate tasks I'm also the tecnical manager and the creator of the website: [https://edifcernusco.it](https://edifcernusco.it)
 `
-	},
+	}, {
+	    title: 'Playground',
+	    date: 'June, 2014',
+	    contents: `
+I took part in an internship lasting one month at Playground ([https://playground.it](https://playground.it)) where I started to approach the web developement.
+`
+	}, {
+	    title: 'Connect.ie',
+	    date: 'June, 2014',
+	    contents: `
+I took part in this internship at Connects.ie (https://www.connect.ie/), the "Ireland's oldest independent service provider", where I developed a web scraping bot. Duration of the experience: two weeks.
+`
+	}
     ],
+
     projects: [
 	{
-	    title: 'BrainFuck compiler',
-	    link: 'https://git.io/JYa2x',
-	    contents: `Realized as divertissement, this small and naive compiler is written in Python. It parses the BrainFuck estoric language generating valid Python bytecode.`
-	},
-    ],
-    skills: `
-I can stare a blinking pointer for hours and I can lick my elbow. And only one of these twos is true.
-`,
-    interests: `
-I like to run
+	    title: 'Materia DeX',
+	    contents: `
+An open source and innovative *Decentralized Exchange* living on *Ethereum*, I mainly wrote the *Solidity* contracts and helped with the frontend integration.
 `
+	}, {
+	    title: 'Cooper algorithm',
+	    contents: `
+Implemented in *C* as future part of *MCMT*, a model checker for infinite state systems based on the integration of Satisfiability Modulo Theories (SMT) solving and backward reachability.`
+	}, {
+	    title: 'LOGO interpreter',
+	    contents: `
+Simple proof of concept for an University course, written in *Python*.`
+	}, {
+	    title: 'BrainFuck compiler',
+	    contents: `
+Realized as divertissement, this small and naive compiler is written in Python. It parses the BrainFuck estoric language generating valid Python bytecode.
+Here the Gist: [https://git.io/JYa2x](https://git.io/JYa2x)`
+	}, {
+	    title: 'My blog',
+	    contents: `
+A simple container for my thoughts and experiments, having no requirements I have the maximum freedom in the technologic choises. The contents are versioned using Git and the generation is done using *Haskell* and *Nix*.
+Here the GitHub repository: [https://github.com/aciceri/blog](https://github.com/aciceri/blog)`
+	}	
+    ],
+    
+    skills: [
+	{
+	    title: 'Languages',
+	    contents: `
+In the professional field I worked with *Python* (Django), *JavaScript* and *Solidity*.
+For personal projects I used functional languages as *Haskell* or *Scheme*, of which I love the expressiveness.
+As student I extensively used *C*, I've also have some rudiments of *Java*, *C Sharp* and *PHP*.`
+	}, {
+	    title: 'Techonologies',
+	    contents: `
+I've experience with *DevOps* using *Git* (GitHub, GitLab) and *Docker*.
+I've a long experience with *POSIX* systems and I'm a *Nix(Os)* enthusiast since I love its declarativeness and reproducible builds.
+My appreciation of decentralized systems led me to study of *Ethereum* and *IPFS*, allowing me to work in the context of *Dapps* and *Decentralized Finance*.
+`
+	}, {
+	    title: 'Softwares',
+	    contents: `
+I've been using GNU/Linux for years, I'm comfortable with the shell and I'm used to customize my environment for greater efficiency.
+Moreover I extensively use *Emacs*, not just as a text editor. Other softwares I've dealt with in the past, in no particular order, includes *Gimp*, *Blender*, *Matlab*, *LaTeX*, *MySQL*, *SQLite*, *ANTLR* and *Arduino*.
+`
+	}
+    ],
+    
+    interests: `
+As person who studied math and computer science I enjoy analyzing and formalizing problems and puzzles looking for optimal solutions, this applies to every aspect of my life, even my personal life.
+When I was younger I studied music, and even if now only as a listener, I continue to marvel at its expressive possibilities in all its genres. I made this interest collide with my attitude tried programming simple sequencers and synthesizers. 
+I'm fond of science fiction books but sometimes I also enjoy the great classics, I also like Cinema. 
+It's been a few years that I run and I've even partecipated in some races, I think that devote few hours a week to my pshysical well-being allows for a greater mental efficiency.
+`,
+    
+    languages: `
+I 'm native Italian speaker and I've a solid command of English, in both written and spoken language. As developer I use to read and write technical documents and I actively partecipate in several online community.
+As student I've been in England and Ireland studying English where I improved my ability to hold face-to-face discussions, I'm extremely prone to improve my fluency. 
+`,
+    education: `Currently I'm a math student at the University of Milan, futhermore I've an high school diploma in information technology and telecommunications.`
 }

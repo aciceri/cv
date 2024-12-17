1edf87ae9f1ba89c4318ffac29fce5e74568e0ec
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-12-17 12:02:47 +0100

    New CV

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Andrea Ciceri's Curriculum Vitae";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, config, lib, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+	    texlive.combined.scheme-full
+	  ];
+        };
+	packages = {
+	  cv = pkgs.runCommandNoCC "cv.pdf" {} ''
+	    cp -r ${./src}/* . 
+            ${lib.getExe' pkgs.texlive.combined.scheme-full "xelatex"} cv.tex
+	    mkdir $out
+            cp {cv.pdf,index.html} $out
+	  '';
+	  default = config.packages.cv;
+	};
+      };
+    };
+}

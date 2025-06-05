9cbbac5342edb6b1a3adc9916fa0d8a798216c67
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-06-05 11:04:55 +0200

    Format nix code

--- flake.nix
+++ flake.nix
@@ -6,24 +6,33 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, config, lib, ... }: {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-	    texlive.combined.scheme-full
-	  ];
+      perSystem =
+        {
+          pkgs,
+          config,
+          lib,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              texlive.combined.scheme-full
+            ];
+          };
+          packages = {
+            cv = pkgs.runCommandNoCC "cv.pdf" { } ''
+              cp -r ${./src}/* . 
+              ${lib.getExe' pkgs.texlive.combined.scheme-full "xelatex"} cv.tex
+              mkdir $out
+              cp {cv.pdf,index.html} $out
+            '';
+            default = config.packages.cv;
+          };
+          formatter = pkgs.nixfmt-tree;
         };
-	packages = {
-	  cv = pkgs.runCommandNoCC "cv.pdf" {} ''
-	    cp -r ${./src}/* . 
-            ${lib.getExe' pkgs.texlive.combined.scheme-full "xelatex"} cv.tex
-	    mkdir $out
-            cp {cv.pdf,index.html} $out
-	  '';
-	  default = config.packages.cv;
-	};
-      };
     };
 }
